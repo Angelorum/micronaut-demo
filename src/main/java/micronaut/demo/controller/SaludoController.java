16927651533b54509cdf53938718fa08c7510a6e
Angelorum <angelo.ramos.o@gmail.com>
2018-10-09 02:59:15 -0500

    prueba inicial

--- src/main/java/micronaut/demo/controller/SaludoController.java
+++ src/main/java/micronaut/demo/controller/SaludoController.java
@@ -1,4 +1,17 @@
 package micronaut.demo.controller;
 
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/saludo")
 public class SaludoController {
+
+    @Get("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String base() {
+        return "Hello World";
+    }
+
 }
